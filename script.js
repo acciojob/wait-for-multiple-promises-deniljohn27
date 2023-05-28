618139f3c847f1f551a5fe8c37f6d396bb0c35d6
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-29 00:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let body=document.getElementById("output");
 let row=document.createElement("tr");
 let col=document.createElement("td");
 col.innerText="Loading..."
+col.id="loading"
 col.colSpan=2;
 row.append(col);
 body.append(row);
@@ -10,7 +11,7 @@ body.append(row);
 let proArr=[];
 let pro1=new Promise((resolve)=>{setTimeout(()=>{resolve(1)},1000)})
 let pro2=new Promise((resolve)=>{setTimeout(()=>{resolve(2)},3000)})
-let pro3=new Promise((resolve)=>{setTimeout(()=>{resolve(3)},4000)})
+let pro3=new Promise((resolve)=>{setTimeout(()=>{resolve(3)},2000)})
 
 proArr.push(pro1);
 proArr.push(pro2);
