9a3d8f3f28c59706b4192dd7e9f9babfd5f701ca
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-28 23:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let body=document.getElementById("output");
+let row=document.createElement("tr");
+let col=document.createElement("td");
+col.innerText="Loading..."
+col.colSpan=2;
+row.append(col);
+body.append(row);
+
+let proArr=[];
+let pro1=new Promise((resolve)=>{setTimeout(()=>{resolve(1)},2000)})
+let pro2=new Promise((resolve)=>{setTimeout(()=>{resolve(2)},3000)})
+let pro3=new Promise((resolve)=>{setTimeout(()=>{resolve(3)},4000)})
+
+proArr.push(pro1);
+proArr.push(pro2);
+proArr.push(pro3);
+
+pro1.then((con)=>{
+	let body=document.getElementById("output");
+	let row=document.createElement("tr");
+	let col=document.createElement("td");
+	col.innerText="Loading..."
+	col.colSpan=2;
+	row.append(col);
+	body.append(row);
+})
